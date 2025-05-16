50aabe6a81fcbdff71fb4ffcdcb1c825868cef00
EC2 Default User <ec2-user@ip-172-31-5-168.ap-south-1.compute.internal>
2025-05-16 07:02:05 +0000

    updated file1.java

--- file1.java
+++ file1.java
@@ -4,6 +4,6 @@ public class File1{
 		Scanner sc = new Scanner(System.in);
 		int a= sc.nextInt();
 		int b= sc.nextInt();
-		System.out.println("Sum of 2 numbers:"+(a+b));
+		System.out.println("Difference of 2 numbers:"+(a-b));
 	}
 }
