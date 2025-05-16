774f87b111cc7694abca0c7727bbdaeb5034f118
EC2 Default User <ec2-user@ip-172-31-5-168.ap-south-1.compute.internal>
2025-05-16 06:43:39 +0000

    Added file1 to the project

---
+++ file1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class File1{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int a= sc.nextInt();
+		int b= sc.nextInt();
+		System.out.println("Sum of 2 numbers:"+(a+b));
+	}
+}
